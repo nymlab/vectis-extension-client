68f73b61ac7456a19e1146fde15a8cc85a94f235
j0nl1 <jonlitech@gmail.com>
2023-09-16 16:16:53 +0200

    fix: vectis extension for cosmjs
    feat: add content policy
    
    csp for cloudflare
    
    wip

--- lib/build/esm/types/extensions/cosmjs.d.ts
+++ lib/build/esm/types/extensions/cosmjs.d.ts
@@ -1,15 +1,10 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
-import { StdFee } from '@cosmjs/amino';
-import { GasPrice, SignerData } from '@cosmjs/stargate';
-import { OfflineSigner, EncodeObject } from '@cosmjs/proto-signing';
-import { TxRaw } from '../types/cosmos';
+import { GasPrice } from '@cosmjs/stargate';
+import { OfflineDirectSigner } from '@cosmjs/proto-signing';
 interface CosmWasmVectisExtension extends SigningCosmWasmClient {
-    _signer: OfflineSigner;
+    _signer: OfflineDirectSigner;
     _gasPrice: GasPrice;
     _signAndBroadcast: SigningCosmWasmClient['signAndBroadcast'];
-    _sign: SigningCosmWasmClient['sign'];
-    _simulate: SigningCosmWasmClient['simulate'];
-    _signDirect: (signerAddress: string, messages: readonly EncodeObject[], fee: StdFee, memo: string, { accountNumber, sequence, chainId }: SignerData) => Promise<TxRaw>;
 }
 export declare function setupWithVectisExtension(client: SigningCosmWasmClient): SigningCosmWasmClient & CosmWasmVectisExtension;
 export {};
--- lib/src/extensions/cosmjs.ts
+++ lib/src/extensions/cosmjs.ts
@@ -1,85 +1,53 @@
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
 import { StdFee } from '@cosmjs/amino';
-import { DeliverTxResponse, GasPrice, SignerData, calculateFee } from '@cosmjs/stargate';
-import { OfflineSigner, EncodeObject } from '@cosmjs/proto-signing';
-import { assertDefined } from '@cosmjs/utils';
-import { TxRaw } from '../types/cosmos';
+import { DeliverTxResponse, GasPrice } from '@cosmjs/stargate';
+import { OfflineSigner, EncodeObject, OfflineDirectSigner, encodePubkey, makeAuthInfoBytes, makeSignDoc } from '@cosmjs/proto-signing';
 import { VectisAccountData } from '../types';
 
 interface CosmWasmVectisExtension extends SigningCosmWasmClient {
-  _signer: OfflineSigner;
+  _signer: OfflineDirectSigner;
   _gasPrice: GasPrice;
   _signAndBroadcast: SigningCosmWasmClient['signAndBroadcast'];
-  _sign: SigningCosmWasmClient['sign'];
-  _simulate: SigningCosmWasmClient['simulate'];
-  _signDirect: (
-    signerAddress: string,
-    messages: readonly EncodeObject[],
-    fee: StdFee,
-    memo: string,
-    { accountNumber, sequence, chainId }: SignerData
-  ) => Promise<TxRaw>;
 }
 
 export function setupWithVectisExtension(client: SigningCosmWasmClient): SigningCosmWasmClient & CosmWasmVectisExtension {
   const cwclient = Object.assign(client, {
     _signAndBroadcast: client.signAndBroadcast,
-    _simulate: client.simulate,
-    _sign: client.sign,
-    // @ts-expect-error
-    _signDirect: client.signDirect,
     // @ts-expect-error
     _signer: client.signer as OfflineSigner,
     // @ts-expect-error
     _gasPrice: client.gasPrice as GasPrice,
-    async simulate(signerAddress: string, messages: readonly EncodeObject[], memo = ''): Promise<number> {
-      const [{ type }] = (await this._signer.getAccounts()) as unknown as VectisAccountData[];
-      if (!['vectis', 'ica'].includes(type)) return await this._simulate(signerAddress, messages, memo);
-      return Promise.resolve(0);
-    },
     async signAndBroadcast(
       signerAddress: string,
       messages: readonly EncodeObject[],
       fee: StdFee | 'auto' | number,
       memo = ''
     ): Promise<DeliverTxResponse> {
-      const [{ type }] = (await this._signer.getAccounts()) as unknown as VectisAccountData[];
-      if (!['vectis', 'ica'].includes(type)) return await this._signAndBroadcast(signerAddress, messages, fee, memo);
-      let usedFee: StdFee;
-      if (fee == 'auto' || typeof fee === 'number') {
-        assertDefined(this._gasPrice, 'Gas price must be set in the client options when auto gas is used.');
-        const gasEstimation = await this.simulate(signerAddress, messages, memo);
-        // Starting with Cosmos SDK 0.47, we see many cases in which 1.3 is not enough anymore
-        // E.g. https://github.com/cosmos/cosmos-sdk/issues/16020
-        const multiplier = typeof fee === 'number' ? fee : 1.4;
-        usedFee = calculateFee(Math.round(gasEstimation * multiplier), this._gasPrice);
-      } else {
-        usedFee = fee;
-      }
-      const txDelivered = (await this.sign(signerAddress, messages, usedFee, memo)) as unknown as DeliverTxResponse;
-      return txDelivered;
-    },
-    async sign(
-      signerAddress: string,
-      messages: readonly EncodeObject[],
-      fee: StdFee,
-      memo: string,
-      explicitSignerData?: SignerData
-    ): Promise<TxRaw> {
-      const [{ type }] = (await this._signer.getAccounts()) as unknown as VectisAccountData[];
-      if (!['vectis', 'ica'].includes(type)) return await this._sign(signerAddress, messages, fee, memo, explicitSignerData);
-      let signerData: SignerData;
-      if (explicitSignerData) {
-        signerData = explicitSignerData;
-      } else {
-        const chainId = await this.getChainId();
-        signerData = {
-          accountNumber: 0,
-          sequence: 0,
-          chainId: chainId
-        };
-      }
-      return this._signDirect(signerAddress, messages, fee, memo, signerData);
+      const [{ type, pubkey: rawPubKey }] = (await this._signer.getAccounts()) as unknown as VectisAccountData[];
+      if (!['ica'].includes(type)) return await this._signAndBroadcast(signerAddress, messages, fee, memo);
+      this._gasPrice.denom;
+
+      const pubkey = encodePubkey({
+        type: 'tendermint/PubKeySecp256k1',
+        value: btoa(String.fromCharCode(...(rawPubKey as unknown as Uint8Array)))
+      });
+
+      const txBody = {
+        typeUrl: '/cosmos.tx.v1beta1.TxBody',
+        value: {
+          messages: messages,
+          memo: memo
+        }
+      };
+
+      const txBodyBytes = this.registry.encode(txBody);
+      const chainId = await this.getChainId();
+      const granter = typeof fee === 'object' ? fee.granter : undefined;
+      const payer = typeof fee === 'object' ? fee.payer : undefined;
+
+      const authInfoBytes = makeAuthInfoBytes([{ pubkey, sequence: 0 }], [{ denom: this._gasPrice.denom, amount: '0' }], 0, granter, payer);
+      const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, chainId, 0);
+      return (await this._signer.signDirect(signerAddress, signDoc)) as unknown as DeliverTxResponse;
     }
   } as SigningCosmWasmClient & CosmWasmVectisExtension);
   return cwclient;
